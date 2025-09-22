02a63a87a5397a3127fa42d546f323face390206
Katerina Kazandzhieva <katerina.kazandzhieva@gmail.com>
2025-09-22 17:08:44 +0300

    Update guess_a_number.py
    Added levels, this should be fun.

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,22 +1,7 @@
 import random
 
-def play_game():
-    computer_number = random.randint(1, 100)
-    print("Hello, dear friend! Want to play a game?")
-    print("Can you guess the number i have generated? It should be between 1 and 100 :)")
-
-    while True:
-        try:
-            original_number_of_tries = int(input("You have n tries to guess it. Please enter n: "))
-            if original_number_of_tries <= 0:
-                print("Please enter a positive number of tries, dum dum.")
-                continue
-            break
-        except ValueError:
-            print("Sorry, lad, wrong kinda input. Try again?")
-
-
-
+def play_game(max_number, original_number_of_tries):
+    computer_number = random.randint(1, max_number)
     number_of_tries_left = original_number_of_tries
 
     while number_of_tries_left > 0:
@@ -34,7 +19,7 @@ def play_game():
 
         if player_number == computer_number:
             print(f"Way to go! You guessed it!. And it only took {original_number_of_tries - number_of_tries_left + 1} tries!")
-            return
+            return True
 
         elif player_number > computer_number:
             print("Ooops, too high! Better get your head out of the clouds!")
@@ -46,14 +31,33 @@ def play_game():
 
     print(f"Oh no! You are all out of tries! The number was: {computer_number}")
 
+max_number = 100
+level = 1
+
 while True:
-    play_game()
-    answer = input("Wanna play again? (Yes or No pls): ").strip().lower()
-    if answer != "yes":
-        print("Game over, noob. Shutting down.")
-        break
-    else:
-        print("NEW GAME")
-        print("\n")
-        print("Welcome back! Muahahahaha")
+    print("Hello, dear friend! Want to play a game?")
+    print(f"Can you guess the number i have generated? It should be between 1 and {max_number} :)")
+    print(f"---Level {level}---")
+    while True:
+        try:
+            original_number_of_tries = int(input("You have n tries to guess it. Please enter n: "))
+            if original_number_of_tries <= 0:
+                print("Please enter a positive number of tries, dum dum.")
+                continue
+            break
+        except ValueError:
+            print("Sorry, lad, wrong kinda input. Try again?")
+
+    win = play_game(max_number, original_number_of_tries)
+
+    if win:
+        level += 1
+        max_number += 100
+
+        answer = input("Wanna play again? (Yes or No pls): ").strip().lower()
+        if answer != "yes":
+            print(f"Thanks for playing! You made it to level {level}.")
+            break
+        else:
+            print("Welcome back! Muahahahaha")
 
