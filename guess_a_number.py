c652216d15d2c673eb74867b93a90fc1f2e9aefb
Katerina Kazandzhieva <katerina.kazandzhieva@gmail.com>
2025-09-22 13:17:53 +0300

    Update guess_a_number.py
    So, did a little bit of research. This kinda looks better.

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,16 +1,25 @@
 import random
 
-game_on = True
 
-while game_on is True:
+
+def play_game():
     computer_number = random.randint(1, 100)
-    print("Hello, dear friend! Want to play a game? Can you guess the number i have generated? It should be between 1 and 100 :)")
-    print("You have n tries to guess it. Please enter n:", end="")
-    original_number_of_tries = input()
-    if not original_number_of_tries.isdigit():
-        print("Sorry, lad, wrong kinda input. Try again?")
-        continue
-    number_of_tries_left = int(original_number_of_tries)
+    print("Hello, dear friend! Want to play a game?")
+    print("Can you guess the number i have generated? It should be between 1 and 100 :)")
+
+    while True:
+        try:
+            original_number_of_tries = int(input("You have n tries to guess it. Please enter n: "))
+            if original_number_of_tries <= 0:
+                print("Please enter a positive number of tries, dum dum.")
+                continue
+            break
+        except ValueError:
+            print("Sorry, lad, wrong kinda input. Try again?")
+
+
+
+    number_of_tries_left = original_number_of_tries
 
     while number_of_tries_left > 0:
         player_input = input("Gimme a number:")
@@ -28,9 +37,7 @@ while game_on is True:
 
         if player_number == computer_number:
             print(f"Way to go! You guessed it!. And it only took {original_number_of_tries - number_of_tries_left + 1} tries!")
-            game_on = False
-            print("Play again? (Yes/No)")
-            break
+            return
 
         elif player_number > computer_number:
             print("Ooops, too high! Better get your head out of the clouds!")
@@ -40,15 +47,16 @@ while game_on is True:
 
         number_of_tries_left -= 1
 
-    if number_of_tries_left == 0:
-        game_on = False
-        print("Oh no! You are all out of tries! Play again? (Yes/No)")
+    print(f"Oh no! You are all out of tries! The number was: {computer_number}")
 
-    if game_on is False:
-        answer = input()
-        if answer == "Yes":
-            game_on = True
-        elif answer == "No":
-            print("Game over, noob. Shutting down.")
-            break
+while True:
+    play_game()
+    answer = input("Wanna play again? (Yes or No pls): ").strip().lower()
+    if answer != "yes":
+        print("Game over, noob. Shutting down.")
+        break
+    else:
+        print("NEW GAME")
+        print("\n")
+        print("Welcome back! Muahahahaha")
 
